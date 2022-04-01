e2908f9fa9839bb9c6cd466ba1c8ca17d60b7b47
Maximilian Litteral <maxlitteral@gmail.com>
2022-04-01 11:00:58 -0400

    Initial Commit

---
+++ Shared/LegoDimensionsToyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoDimensionsToyPadApp.swift
+//  Shared
+//
+//  Created by Maximilian Litteral on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LegoDimensionsToyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
